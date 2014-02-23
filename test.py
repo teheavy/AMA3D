bafff1615a7cf9549430e6a75054540a6bf59a7c
hyginn <boris.steipe@utoronto.ca>
2014-02-22 21:48:39 -0500

    Test
    Create and commit a test file.
    
    Signed-off-by: hyginn <boris.steipe@utoronto.ca>

---
+++ test.py
@@ -0,0 +1,12 @@
+# test.py
+# simple file to play with to exercise IDE, git and github configs.
+# change at will.
+#
+# bs
+
+
+print 'Frustra fit per plura, quod potest fieri per pauciora.'
+print 'W. Occam\n\n'
+
+
+
