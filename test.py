ea237268632bf10f9a065c1d4e9c97f1d18092de
Hadiparsa <lahijani1@gmail.com>
2014-03-04 10:13:48 -0500

    I changed one line in test.py
    test for windows GUI

--- test.py
+++ test.py
@@ -5,6 +5,7 @@
 # bs
 # this is vaishali, changing the pytest file.
 # Hadi added something
+# now I'm working with Git GUI in widows
 
 print 'Frustra fit per plura, quod potest fieri per pauciora.'
 print 'W. Occam\n\n'
