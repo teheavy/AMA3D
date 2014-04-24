daecf822059efc650771a162af87fe5c601e9179
ehsueh <ehsueh1993@gmail.com>
2014-04-24 00:21:34 -0400

    restructure: external py for global vars

---
+++ AMA_globls.py
@@ -0,0 +1,12 @@
+# AMA global variables
+
+VERSION = "1.0.0"
+# This version assumes a specific database and relation format according to the entity-relational diagram 
+# presented in AMA3D
+AGENT_ID = ""
+DIE = False #
+ADMINFILE = "" # file storing admin info
+MYEMAIL = "" # email account of AMA3D
+PARAM = "" # the data this agent is using right now
+DB = "" # global db connection
+
