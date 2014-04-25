e1a6df9ccfe9d88ea6172da01b0f4ac6f87622f1
ehsueh <ehsueh1993@gmail.com>
2014-04-25 16:07:47 -0400

    Update AMA_globls.py

--- AMA_globls.py
+++ AMA_globls.py
@@ -9,4 +9,14 @@ ADMINFILE = "" # file storing admin info
 MYEMAIL = "" # email account of AMA3D
 PARAM = "" # the data this agent is using right now
 DB = "" # global db connection
+MACHINE_ID = 0 # the id of the machine the agent is on currently
 
+#TODO
+# 1. make machine registry table called (Machines)
+# machine id (idMachine)
+# abs path of where the AMA3D folder is downloaded (Path)
+# CPU memory and other machine info
+# username (User)
+# password salted and hashed (Pw)
+
+# 2. update relation TaskResource (add column Program)
