15edfc04f326e1dd86bf36c9cb3ecf7228a21263
teheavy <fredchen666@gmail.com>
2014-06-05 16:18:22 -0400

    update host addr

--- AMA_globals.py
+++ AMA_globals.py
@@ -10,13 +10,4 @@ MYEMAIL = "" # email account of AMA3D
 PARAM = "" # the data this agent is using right now
 DB = "" # global db connection
 MACHINE_ID = 0 # the id of the machine the agent is on currently
-
-#TODO
-# 1. make machine registry table called (Machines)
-# machine id (idMachine)
-# abs path of where the AMA3D folder is downloaded (Path)
-# CPU memory and other machine info
-# username (User)
-# password salted and hashed (Pw)
-
-# 2. update relation TaskResource (add column Program)
+HOST=142.150.40.189
